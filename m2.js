f40fc3a6a7a73d89f1c56075c56016c5db8eb5f7
nnisyaa <n.hasratova@mail.ru>
2022-11-10 12:18:34 +0400

    Third Push

---
+++ m2.js
@@ -0,0 +1,46 @@
+const greyArrowDown = document.querySelector('.greyArrowDown');
+
+greyArrowDown.addEventListener('mouseover', (event) => {
+    event.target. = '';
+});
+greyArrowDown.addEventListener('mouseout', (event) => {
+    event.target. = 'green';
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let addContex= document.querySelector('.')
+let lists = document.querySelector('.newAddSpaces')
+let input = document.querySelector('.')
+
+
+
+let array = []
+addContex.onclick= function(){
+    if (input.value.length==0){
+        alert('The space cant be empty')
+    }
+    else{
+        newAddSpaces.innerHTML +=
+
+    }
+}
+
+
+
+
+
+
