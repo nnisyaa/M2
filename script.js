33961557eb2ba9511dc125f4802339ac8fe45aca
nnisyaa <n.hasratova@mail.ru>
2022-11-16 09:55:09 +0400

    Final

--- script.js
+++ script.js
@@ -1,40 +1,24 @@
+const textInputField = document.querySelector
+('#text-input-field');
+const addButton = document.querySelector('#add-button');
+const secondContainer =  document.querySelector('.second-container');
 
-let reverseButton = document.querySelector('.greyArrowDown')
-let sortButton = document.querySelector('.greyArrowUp')
-
-
-
-
-
-
-const button = document.querySelector('button');
-button.onclick = function () {
-    div = document.createElement('div')
-    div.innerHTML = `
-    <input class="input-space" name="input" type="text" required>
-    <button class="greyx"><img src="/image/GreyX.svg"  alt="Not found" class="greyx-picture"></button>
-    `
-}
+addButton.addEventListener('click',()=>{
+    if(textInputField.ariaValueMax.trim().length=='')
+    return;
 
 
 
+const todoItem = document.createElement('div');
+todoItem.classList.add('todo-item');
+
+secondContainer.appendChild(todoItemContainer);
+
+const todoText = document.createElement('p');;
+todoText.id = 'todo-text';
+todoText.innerText = textInputField.value; 
+todoItem.appendChild(todoText);
 
 
-
-
-
-
-
-
-
-
-
-const arrow  = document.querySelector('.greyArrowDown');
-
-arrow.addEventListener('mouseover', (event) => {
-    event.target.src="/image/greyArrowUp.svg";
-});
-
-arrow.addEventListener('mouseout', (event) => {
-    event.target.src="/image/greyArrowDown.svg" ;
-});
+textInputField = ""
+})
